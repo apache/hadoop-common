ba8dd3bffdc5e55772e2cf23b6e870c4c64b9a1a
Mahadev Konar <mahadev@apache.org>
2011-05-17 18:51:17 +0000

    Fixed Composite Service to shutdown services if an error occurs on starting any one of those (mahadev & chris)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/mapreduce/branches/MR-279@1104464 13f79535-47bb-0310-9956-ffa450edef68

--- yarn/yarn-common/src/main/java/org/apache/hadoop/yarn/service/CompositeService.java
+++ yarn/yarn-common/src/main/java/org/apache/hadoop/yarn/service/CompositeService.java
@@ -1,20 +1,20 @@
 /**
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership.  The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.apache.hadoop.yarn.service;
 
@@ -23,12 +23,17 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.yarn.YarnException;
 
 /**
  * Composition of services.
  */
 public class CompositeService extends AbstractService {
+
+  private static final Log LOG = LogFactory.getLog(CompositeService.class);
 
   private List<Service> serviceList = new ArrayList<Service>();
 
@@ -56,8 +61,23 @@ public class CompositeService extends AbstractService {
   }
 
   public synchronized void start() {
-    for (Service service : serviceList) {
-      service.start();
+    int i = 0;
+    try {
+      for (int n = serviceList.size(); i < n; i++) {
+        Service service = serviceList.get(i);
+        service.start();
+      }
+    } catch(Throwable e) {
+      LOG.error("Error starting services " + getName(), e);
+      for (int j = i-1; j >= 0; j--) {
+        Service service = serviceList.get(j);
+        try {
+          service.stop();
+        } catch(Throwable t) {
+          LOG.info("Error stopping " + service.getName(), t);
+        }
+      }
+      throw new YarnException("Failed to Start " + getName(), e);
     }
     super.start();
   }
